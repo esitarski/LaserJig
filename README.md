85132760e626cf1a8a9bc25c904144a211d30108
esitarski <edward.sitarski@gmail.com>
2021-01-19 13:47:12 -0500

    Sorted the output shapes to make it easier to see if everything is there.

--- README.md
+++ README.md
@@ -2,21 +2,24 @@
 Bike Jig using lasers.  Designed to be used in a fixed location (i.e. a velodrome).
 It is heavy and non-portable.
 The intent is for riders to check their own bikes.  As such, the jig must be "bomb-proof" as it will be subject to
-use and abuse while remaining extremely accurate.  Hence the use of contactless lasers.
+use and abuse while remaining extremely accurate.  Lasers are used as they are accurate and contactless.
 
 Plywood parts are extracted from the OpenSCAD design via the extract.py script and written to dxf and svg files.
 These files can be used by a digital router that can cut the parts out of 19mm birch plywood.
-Assembly is relatively straight-forward with biscuit joinery.  The design is somewhat assembly-friendly and is it
-relatively easy to align the parts on all the critical dimensions just be pushing the boards together.
-Add dimensions are in millimeters.
+Assembly is relatively straight-forward with biscuit joinery.  The design has a number of assembly-friendly features including lips and
+alignment points to make it easier to line up the parts at assembly.
+The laser modules are held in place with adjustable clamps which make it possible to fine-tune the accuracy during assembly.
+Some critial assembly dimensions are also output including the position of the column and laser module.
 
-It is important that the router makes highly accurate cuts otherwise the jig will not fit together.  However, it is possible to overcome some inaccuracies
-by compensating in the final positioning of the lasers.
+It is important that your CNC router makes highly accurate cuts otherwise the jig will not fit together.
+Some inaccuracies can be overcome in the final positioning of the lasers.
 
 The design is parameterized in all key dimensions (see params.scad).  It is easy to change for different sized laser modules etc.
+Add dimensions are in millimeters.
 
-19mm birch plywood is a great material choice.  However, if you want to use a different plywood, just change sheet_thickness
-in the params.scad file.  Be sure to measure the thickness accurately and convert to mm.
+19mm birch plywood is a great material choice.  However, if you wish to use a different plywood, just change "sheet_thickness"
+in the params.scad file.  All aspects of the design will adjust appropriately.
+Be sure to measure the thickness of your material accurately and convert to mm.
 
 Requires OpenSCAD and Python3.
 
--- export.py
+++ export.py
@@ -1,17 +1,14 @@
+import os
 import re
+import shutil
 from collections import defaultdict
 from subprocess import Popen, DEVNULL, PIPE
-import svgwrite
 import ezdxf
-import shutil
-import os
+import svgwrite
 
 cmd = ['openscad', 'main.scad', '-o', 'main.png']
 
-proc = Popen( cmd, stdout=PIPE, stderr=PIPE )
-out, err = proc.communicate()
-
-sheet_thickness = 0
+sheet_thickness = 0		# This comes from the scad file.
 
 board_height, board_width = 8*12*2.54, 4*12*2.54
 
@@ -25,6 +22,13 @@ def cube_to_points( cube ):
 	x, y = points
 	return ((0,0), (x,0), (x,y), (0,y))
 
+# Extract the model information from the scad file.
+proc = Popen( cmd, stdout=PIPE, stderr=PIPE )
+out, err = proc.communicate()
+
+# Parse the lines to get the polygons.
+# Convert cubes to polygons.
+# Also, keep track of the count of each polygon.
 for line in err.decode().split('\n'):
 	if line.startswith('ECHO:') and '=' in line:
 		line = line[5:].strip()
@@ -57,6 +61,9 @@ for line in err.decode().split('\n'):
 for label, points in polys.items():
 	print( '{}[{}]: {}'.format( label, count[label], points ) )
 
+def approx_poly_area( points ):
+	return max(x for x,y in points) * max(y for x,y in points)
+
 #-----------------------------------------------------------------------
 doc = ezdxf.new()
 doc.units = 4	# mm
@@ -69,16 +76,16 @@ def to_poly_dxf( msp, label, points ):
 	dxf_points = [(x_cur + x, y_cur + y) for x, y in points]
 	p = msp.add_lwpolyline( dxf_points )
 	p.close( True )
-	x_cur = max( x for x,y in dxf_points ) + 30
+	x_cur = max( x for x,y in dxf_points ) + 20
 	return p
 
 #-----------------------------------------------------------------------
 # Save all polygons in one dxf format.
 #
-for k,v in polys.items():
+for k,v in sorted( polys.items(), key=lambda e: approx_poly_area(e[1]), reverse=True ):
 	for c in range(count[k]):
 		to_poly_dxf( msp, '{}_{}'.format(k,c+1), v )
-doc.saveas('BikeJig.dxf')
+doc.saveas('LaserJig.dxf')
 
 #-----------------------------------------------------------------------
 # Save all polygons in dxf format, one file for each, in a directory.
@@ -87,7 +94,8 @@ target_dir = 'dxf_files'
 shutil.rmtree( target_dir, ignore_errors=True )
 	
 os.mkdir( target_dir )
-for k,v in polys.items():
+for k,v in sorted( polys.items(), key=lambda e: approx_poly_area(e[1]), reverse=True ):
+
 	for c in range(count[k]):
 		doc = ezdxf.new()
 		doc.units = 4	# mm
@@ -99,22 +107,39 @@ for k,v in polys.items():
 		doc.saveas( os.path.join(target_dir, fname) )
 
 #-----------------------------------------------------------------------
+# Save all polygons in one svg file.
+#
+def to_poly_svg( dwg, label, points ):
+	global x_cur, y_cur
+	y_max = max( y for x,y in points )
+	x_max = max( x for x,y in points )
+	if x_max > y_max:
+		svg_points = [(y + x_cur, x_max - x + y_cur) for x, y in points]
+	else:
+		svg_points = [(x + x_cur, y_max - y + y_cur) for x, y in points]
+	dwg.add( dwg.polyline(points=svg_points, stroke='none', fill='blue') )
+	x_cur = max( x for x,y in svg_points ) + 20
+
+x_cur = y_cur = 0
+dwg = svgwrite.Drawing( 'LaserJig.svg', profile='tiny' )
+for k,v in sorted( polys.items(), key=lambda e: approx_poly_area(e[1]), reverse=True ):
+	for c in range(count[k]):
+		to_poly_svg( dwg, '{}_{}'.format(k,c+1), v )
+dwg.save()
+
+#-----------------------------------------------------------------------
 # Save all polygons in svg format, one file for each, in a directory.
 #
 target_dir = 'svg_files'
 shutil.rmtree( target_dir, ignore_errors=True )
-	
-def to_poly_svg( dwg, label, points ):
-	dwg.add( dwg.polyline(points=points,
-			stroke='none',
-			fill='blue',
-		) )
 
 os.mkdir( target_dir )
-for k,v in polys.items():
+for k,v in sorted( polys.items(), key=lambda e: approx_poly_area(e[1]), reverse=True ):
+	print( approx_poly_area(v) )
 	for c in range(count[k]):
 		fname = '{}_{}.svg'.format(k, c+1) if count[k] > 1 else '{}.svg'.format(k)
 		dwg = svgwrite.Drawing( os.path.join(target_dir, fname), profile='tiny' )
+		x_cur = y_cur = 0
 		to_poly_svg( dwg, '{}_{}'.format(k,c+1), v )
 		print( k, c, count[k], fname )
 		dwg.save()
